5498d3d0126f468a758d7edfe93534f345a4cacf
Paul Bilan <paul@alumni.manchester.ac.uk>
2025-02-27 22:16:13 +0000

    auto commit

--- source/app.py
+++ source/app.py
@@ -20,6 +20,7 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import CrossEncoder
 from streamlit.runtime.uploaded_file_manager import UploadedFile
+from transformers import pipeline
 
 system_prompt = """
 You are an AI assistant tasked with providing detailed answers based solely on the given context. Your goal is to analyze the information provided and formulate a comprehensive, well-structured response to the question.
