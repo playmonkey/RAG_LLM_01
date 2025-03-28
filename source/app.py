515ea63f7e2351d66af3eb84a4dbcb7b86447caa
Paul Bilan <paul@alumni.manchester.ac.uk>
2025-03-28 20:51:47 +0000

    auto commit

--- source/app.py
+++ source/app.py
@@ -5,6 +5,11 @@
 #   Yankee Maharjan
 #
 # ---------------------------------------------------------------------------------------------------------------   
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 import tempfile
 
